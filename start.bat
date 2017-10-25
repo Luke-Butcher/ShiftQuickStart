f62b4b1435524438f09b46f751f70989725e4f7b
Andrew Stanton <AndrewStanton94@users.noreply.github.com>
2017-10-25 18:00:03 +0100

    My start up file
    This opens chrome with tabs for
    + Current and new ITH websites
    + Papercut
    + appsanywhere
    + gmail
    + the library classmark tool
    + The ITH group chat in messenger
    
    It also opens notepad

---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+start chrome "http://ith.port.ac.uk/" "http://printers.port.ac.uk:9191/app" "http://is-web-02.uni.ds.port.ac.uk/app/rota" "https://appsanywhere.port.ac.uk/" "https://mail.google.com/mail/u/0/#inbox" "https://library.port.ac.uk/classmark/" "https://www.messenger.com/t/1097815366974128"
+start notepad
